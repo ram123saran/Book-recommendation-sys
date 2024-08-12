9fe0bc1aff4cdd9b369293d6deade88515428abf
Ram Saran <140152427+ram123saran@users.noreply.github.com>
2024-08-12 12:39:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask,render_template,request
 import pickle
 import numpy as np
 
-popular_df = pickle.load(open('popular.pkl','rb'), encoding='latin1')
+popular_df = pickle.load(open('popular.pkl','rb'))
 pt = pickle.load(open('pt.pkl','rb'))
-books = pickle.load(open('books.pkl','rb'), encoding='latin1')
+books = pickle.load(open('books.pkl','rb'))
 similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
 
 app = Flask(__name__)
